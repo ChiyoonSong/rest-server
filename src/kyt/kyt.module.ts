d8d1e29133aa39905f7ecf9c2face4fd3a6d51c1
scy <cysong.dev@gmail.com>
2022-10-30 22:33:10 +0900

    delete kyt package

--- src/kyt/kyt.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { KytController } from './kyt.controller';
-import { KytService } from './kyt.service';
-
-@Module({
-  controllers: [KytController],
-  providers: [KytService],
-})
-export class KytModule {}
